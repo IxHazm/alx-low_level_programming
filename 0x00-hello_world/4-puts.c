2702d9bb633ac53309039a128654cbe76b6bc475
IxHazm <ixhazm@gmail.com>
2024-04-26 19:35:57 +0300

    added c files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+nclude <stdio.h>
+
+/**
+ * main - Entry point
+ * Description : 'print a sentance to screen'
+ * Return: Always '0' (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
