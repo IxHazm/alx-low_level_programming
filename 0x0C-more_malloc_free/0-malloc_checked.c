a150dd6dd1f4a782288d81fa6f1d56f029da7c56
IxHazm <ixhazm@gmail.com>
2024-05-02 20:34:40 +0300

    meow

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * malloc_checked - Allocates memory using malloc.
+ * @b: The number of bytes to be allocated.
+ *
+ * Return: A pointer to the allocated memory.
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *output;
+
+	output = malloc(b);
+	if (output == NULL)
+		exit(98);
+	else
+		return (output);
+}
